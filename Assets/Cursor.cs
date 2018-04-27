46eb608443f9160d634c7caae796b9d133693b25
Gareth Thomas <severn2j@gmail.com>
2018-04-27 11:30:08 +0100

    Initial raycast added to camera

---
+++ Assets/Cursor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster cameraraycaster;
+    // Use this for initialization
+    void Start () {
+        cameraraycaster = GetComponent<CameraRaycaster>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(cameraraycaster.layerHit);
+	}
+}
