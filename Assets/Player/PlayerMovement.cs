191519f2f8ebf27b6803f289e68e470a0644fa1f
Gareth Thomas <severn2j@gmail.com>
2018-06-07 09:44:21 +0100

    Added Gamepad Movement

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,13 +5,16 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+    
+
     [SerializeField] float walkMoveStopRadius = 0.2f;
 
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-    
-        
+
+    bool isInDirectMode = false;  // TODO consider making static
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -19,14 +22,48 @@ public class PlayerMovement : MonoBehaviour
         currentClickTarget = transform.position;
     }
 
-    // TODO fix issue with click to move and WSAD conflicting and increasing speed
+
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.G)) // G for Gamepad.
+        {
+            isInDirectMode = !isInDirectMode; // toggle mode
+            currentClickTarget = transform.position;
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+        
+
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+
+
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void ProcessMouseMovement()
+    {
         if (Input.GetMouseButton(0))
         {
-            print("Cursor raycast hit Layer: " + cameraRaycaster.layerHit);
+
             switch (cameraRaycaster.layerHit)
             {
                 case Layer.Walkable:
@@ -50,7 +87,6 @@ public class PlayerMovement : MonoBehaviour
         {
             m_Character.Move(Vector3.zero, false, false);
         }
-        
     }
 }
 
